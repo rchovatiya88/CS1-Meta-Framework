981cfcbb2b78835a656e61b5dea590d9578c85be
rchovatiya88 <rchovatiya88@gmail.com>
2022-09-05 04:35:10 +0530

    getting startee

--- src/app/modules/classes/NavmeshTest.ts
+++ src/app/modules/classes/NavmeshTest.ts
@@ -30,11 +30,11 @@ export class NavmeshTest {
     box?.object3D.position.set(0, 1, -5);
     
     const map = document.createElement("a-gltf-model");
-    map.setAttribute("src","https://cdn.glitch.global/656c1d0b-a220-4a8c-8ecb-d01466ec202b/level_design_practice_1_complete.glb?v=1649347395645");
+    map.setAttribute("src","https://cdn.glitch.global/a0f42b6b-5748-4de7-8b7f-f072c068f79e/MoonX_Stage1.glb?v=1653535020925");
     map.setAttribute("id","map");
     
     const navmesh = document.createElement("a-gltf-model");
-    navmesh.setAttribute("src", "https://cdn.glitch.global/656c1d0b-a220-4a8c-8ecb-d01466ec202b/navmesh%20(15).gltf?v=1649347490062");
+    navmesh.setAttribute("src", "https://cdn.glitch.global/a0f42b6b-5748-4de7-8b7f-f072c068f79e/MoonX_VR_NavMesh.glb?v=1657563005123");
     navmesh.id = "navmesh";
     navmesh.setAttribute("visible", false);
     
@@ -42,8 +42,8 @@ export class NavmeshTest {
     CS1.scene.entity.appendChild(navmesh);
 
     //add a-cursor as child of camera
-    //const cursor = document.createElement("a-cursor");
-    //CS1.cam.entity.appendChild(cursor);
+    const cursor = document.createElement("a-cursor");
+    CS1.cam.entity.appendChild(cursor);
     
     //add navmesh constraint to camera
     CS1.cam.entity.setAttribute("simple-navmesh-constraint", "navmesh:#navmesh;fall:0.5;height:1.65;");
